519254602f5acdb53bc2d3d7937684fa7edfe91d
Felix Ruano <felixruanoc@gmail.com>
2020-07-09 17:21:20 -0700

    added under construction

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 const Home = () => (
   <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+    <h1>Page under construction!</h1>
 
     <h3>Hello 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
 
