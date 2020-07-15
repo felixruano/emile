8924ba4131b9f525d357e8f002e9280da4935a4d
Felix Ruano <felixruanoc@gmail.com>
2020-07-14 19:07:50 -0700

    updated homepage

--- pages/Mobile.js
+++ pages/Mobile.js
@@ -107,7 +107,7 @@ function Mobile({ }) {
                 </div>
             </div>
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
+                <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.pexels.com/photos/4143791/pexels-photo-4143791.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
             </div>
         </div >
     );
