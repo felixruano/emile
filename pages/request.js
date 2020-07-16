4a693042dfee321b89a29a62ef5c93a2110f6ebd
Felix Ruano <felixruanoc@gmail.com>
2020-07-15 23:26:56 -0700

    fixed form

--- pages/request.js
+++ pages/request.js
@@ -81,7 +81,7 @@ function Request() {
                             </div>
                             <div className="sm:col-span-2">
                                 <span className="w-full inline-flex rounded-md shadow-sm">
-                                    <button type="button" className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+                                    <button type="submit" className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
                                         Let's talk
                                     </button>
                                 </span>
