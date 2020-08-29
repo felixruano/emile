deccd1fe8e0e7de5eefd2957db31fe3d6c08ccab
Felix Ruano <felixruanoc@gmail.com>
2020-08-29 01:33:50 -0700

    fixed HP mobile course columns

--- components/CoursesListHP.js
+++ components/CoursesListHP.js
@@ -7,7 +7,7 @@ const CourseListHP = ({ courses }) => {
             <div className="pt-12 lg:text-center">
                 <p className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Sample classes for credit</p>
             </div>
-            <div className="mt-12 grid gap-5 max-w-lg mx-auto grid-cols-2 lg:grid-cols-3 lg:max-w-none">
+            <div className="mt-12 grid gap-5 max-w-lg mx-auto grid-cols-1 lg:grid-cols-3 lg:max-w-none">
                 {courses.results.map((course) => (
                     <CourseItem course={course} key={course.uid} />
                 ))}
