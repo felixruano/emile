95cc399e17f700990bde7add664e1273c335357a
Felix Ruano <felixruanoc@gmail.com>
2020-08-29 00:40:21 -0700

    added analytics to fulltime page

--- pages/fulltime.js
+++ pages/fulltime.js
@@ -10,6 +10,13 @@ function Fulltime() {
             <title>Classes for credit</title>
             <meta property="og:title" content="Full-time program" key="title" />
         </Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176731734-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)};
+                gtag('js', new Date());
+                gtag('config', 'UA-176731734-1');
+            </script>
 
         <Header />
 
