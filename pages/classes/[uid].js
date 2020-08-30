9517dfdf7ca01192e9bfe5e3bf3013b1624bd2cb
Felix Ruano <felixruanoc@gmail.com>
2020-08-29 20:07:05 -0700

    added course info to PDP

--- pages/classes/[uid].js
+++ pages/classes/[uid].js
@@ -24,7 +24,7 @@ const Course = ({ course }) => {
 
                 <div className="bg-white overflow-hidden">
                     <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-                        <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"></div>
+                        <div className="hidden lg:block absolute top-0 bottom-0 left-3/4 w-screen"></div>
                         <div className="mx-auto text-base max-w-prose lg:max-w-none">
                             <p className="text-md leading-5 font-medium text-gray-900">
                                 <a href="/classes" className="hover:underline">
@@ -45,16 +45,31 @@ const Course = ({ course }) => {
                                     <rect width="404" height="384" fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
                                 </svg>
                                 <div className="relative text-base mx-auto max-w-prose lg:max-w-none">
-                                    <figure>
-                                        <div className="relative pb-7/12 lg:pb-0">
-                                            <img src="/top_view.jpg" alt="" width="1184" height="1376" className="rounded-lg shadow-lg object-cover object-center absolute inset-0 w-full h-full lg:static lg:h-auto" />
-                                        </div>
-                                        <figcaption className="flex mt-3 text-sm text-gray-500">
-                                            <svg className="flex-none w-5 h-5 mr-2 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-                                            </svg>
-                                        </figcaption>
-                                    </figure>
+                                    <div className="relative text-base max-w-prose mx-auto lg:max-w-none">
+                                        <blockquote className="relative border br-gray-100 bg-white rounded-lg shadow-lg">
+                                            <div className="rounded-t-lg px-6 py-4 sm:px-10 sm:pt-3 sm:pb-8">
+                                                <div className="relative text-lg text-gray-700 leading-7 font-medium mt-8">
+                                                    <p className="mt-6 leading-6 text-sm text-gray-500 font-semibold tracking-wide uppercase">Price</p>
+                                                    <p className="relative font-bold">{(course.data.price)}</p>
+                                                    <hr class="mt-3 border-0 bg-gray-300 text-gray-500 h-px"></hr>
+                                                    <p className="mt-6 leading-6 text-sm text-gray-500 font-semibold tracking-wide uppercase">Course Format</p>
+                                                    <p className="relative font-bold">1:1 live sessions</p>
+                                                    <hr class="mt-3 border-0 bg-gray-300 text-gray-500 h-px"></hr>
+                                                    <p className="mt-6 leading-6 text-sm text-gray-500 font-semibold tracking-wide uppercase">Semester Length</p>
+                                                    <p className="relative font-bold">Twenty 90-min sessions, 2-3 sessions per week for 7-10 weeks</p>
+                                                    <hr class="mt-3 border-0 bg-gray-300 text-gray-500 h-px"></hr>
+                                                    <p className="mt-6 leading-6 text-sm text-gray-500 font-semibold tracking-wide uppercase">Start Date</p>
+                                                    <p className="relative font-bold">Oct 1, Nov 1, Dec 1</p>
+                                                    <p className="relative font-light text-sm text-gray-500">Rolling Admissions with flexible start dates also available</p>
+                                                </div>
+                                            </div>
+                                            <div className="p-3">
+                                                <a href="/request" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                                                    Get Started
+                                                    </a>
+                                            </div>
+                                        </blockquote>
+                                    </div>
                                 </div>
                             </div>
                             <div>
@@ -67,8 +82,8 @@ const Course = ({ course }) => {
                                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                     <div className="rounded-md shadow">
                                         <a href="/request" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                                            Request Info
-                  </a>
+                                            Get Started
+                                        </a>
                                     </div>
                                 </div>
                             </div>
