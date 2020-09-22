ef421ea041b43422b17a2da283847b146526abf2
Jonathan Quiros <jonathanquirosp@gmail.com>
2020-09-22 08:07:50 -0700

    Updated 'Our Classes' href to /classes

--- pages/Mobile.js
+++ pages/Mobile.js
@@ -194,7 +194,7 @@ function Mobile({}) {
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
-                  <Link href="/explore">
+                  <Link href="/classes">
                     <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
                       Our Classes
                     </a>
