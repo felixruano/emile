f71fa072d7ee25b0ed13e4f9a7dec044b086c162
Jonathan Quiros <jonathanquirosp@gmail.com>
2020-11-08 12:16:17 -0800

    Initialized firebase analytics

--- utils/firebase/firebaseClient.js
+++ utils/firebase/firebaseClient.js
@@ -10,12 +10,13 @@ const firebaseConfig = {
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    appID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     storageBucket: 'vaultprepmvp.appspot.com',
 };
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
 }
 
 const app = firebase.app();
