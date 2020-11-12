63ebe2247d5dfb797f94b1f45599c7e4d35d4926
Jonathan Quiros <jonathanquirosp@gmail.com>
2020-11-11 19:03:18 -0800

    Fixed circle dot pattern overlap

--- components/background-patterns/CircleDotPattern.tsx
+++ components/background-patterns/CircleDotPattern.tsx
@@ -1,7 +1,7 @@
 const CircleDotPattern = () => (
     <div
         className="absolute hidden h-40 bg-white border-4 border-yellow-300 rounded-b-full lg:block w-80"
-        style={{ zIndex: 10 }}
+        style={{ zIndex: 9 }}
     >
         <img className="w-full h-full" src="/dots.png" />
     </div>
