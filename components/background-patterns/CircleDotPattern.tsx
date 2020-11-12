f71d75ff23e22137ff7b8703ee97296291aadf0b
Jonathan Quiros <jonathanquirosp@gmail.com>
2020-11-11 16:50:53 -0800

    Fixed bug

--- components/background-patterns/CircleDotPattern.tsx
+++ components/background-patterns/CircleDotPattern.tsx
@@ -1,5 +1,8 @@
 const CircleDotPattern = () => (
-    <div className="absolute hidden h-40 bg-white border-4 border-yellow-300 rounded-b-full lg:block w-80" style={{ zIndex: '2'}}>
+    <div
+        className="absolute hidden h-40 bg-white border-4 border-yellow-300 rounded-b-full lg:block w-80"
+        style={{ zIndex: 10 }}
+    >
         <img className="w-full h-full" src="/dots.png" />
     </div>
 );
